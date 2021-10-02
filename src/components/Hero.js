dbf59618076c70fd18717a7e891caf459f490d19
Adam Lang <adam.lang-1@hotmail.com>
2021-10-02 17:52:23 +0200

    feat: hero image done and hero text responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../media/bg.jpeg';
 
 const Hero = () => {
     return (
@@ -7,6 +8,7 @@ const Hero = () => {
                 Hi, I'm Adam Lang, I am a web developer and an aspiring
                 fullstack developer.
             </h1>
+            <img className='bg' src={logo} alt='bg-img' />
         </div>
     );
 };
