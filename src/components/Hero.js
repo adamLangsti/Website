2e1bc1fb8d379fecd98c828fb842a4e3524a7f23
Adam Lang <adam.lang-1@hotmail.com>
2021-10-18 21:25:27 +0200

    feat: content data now has header and subheader in parent element

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import logo from '../media/geo.jpg';
 
 const Hero = () => {
     return (
         <>
             <div className='container'>
-                <h1 className='header'>
-                    Hi, I'm Adam Lang, I am a web developer and an aspiring
-                    fullstack developer.
-                </h1>
-                <button className='abt-btn'>About</button>
-                {/* <img className='bg' src={logo} alt='bg-img' /> */}
+                <aside className='container-data'>
+                    <h1 className='header'>Hello.</h1>
+                    <h2 className='content'>
+                        I'm Adam Lang, I am a web developer and an aspiring
+                        fullstack developer.
+                    </h2>
+                    <button className='abt-btn'>About</button>
+                </aside>
             </div>
             <div className='quarter' />
         </>
