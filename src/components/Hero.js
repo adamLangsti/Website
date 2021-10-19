c755f5ce40773a35ad0a3b08d9b703ca05bdc442
Adam Lang <adam.lang-1@hotmail.com>
2021-10-19 14:57:34 +0200

    fix: removed fragment wrapper

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <>
-            <div className='container'>
-                <aside className='container-data'>
-                    <h1 className='header'>Hello.</h1>
-                    <h2 className='content'>
-                        I'm Adam Lang, I am a web developer and an aspiring
-                        fullstack developer.
-                    </h2>
-                    <button className='abt-btn'>About</button>
-                </aside>
-            </div>
-            <div className='quarter' />
-        </>
+        <div className='container'>
+            <aside className='container-data'>
+                <h1 className='header'>Hello.</h1>
+                <h2 className='content'>
+                    I'm Adam Lang, I am a web developer and an aspiring
+                    fullstack developer.
+                </h2>
+                <button className='abt-btn'>Learn more</button>
+            </aside>
+        </div>
     );
 };
 
