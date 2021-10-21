23e680db54748c121b724db65a2ff1cc26f941c1
Adam Lang <adam.lang-1@hotmail.com>
2021-10-21 10:54:33 +0200

    feat: learn more button has scroll function

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Hero = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        window.scrollY > 300 ? setIsVisible(true) : setIsVisible(false);
+    };
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 100,
+            behavior: 'smooth',
+        });
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     return (
         <div className='container'>
             <aside className='container-data'>
@@ -9,9 +27,9 @@ const Hero = () => {
                     I'm Adam Lang, I am a web developer and an aspiring
                     fullstack developer.
                 </h2>
-                <button className='more-btn'>
+                <button className='more-btn' onClick={scrollToTop}>
                     <h3 className='more-btn-text'>Learn more</h3>
-                    <span className='more-btn-arrow'>➜</span>
+                    <span className='more-btn-arrow'>⇩</span>
                 </button>
             </aside>
         </div>
---
+++ src/components/about-data/about.js
@@ -0,0 +1,3 @@
+export const about = `I have always liked technology and in 2018 I got the chance to study app development 
+where I graduated in 2020.
+`;
